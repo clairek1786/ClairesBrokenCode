2102b87ef77dedc8cd3f61398d8bd9ae93f150f5
clairek1786 <98114387+clairek1786@users.noreply.github.com>
2022-03-02 14:41:48 -0700

    Update brokenpythoncode.py

--- brokenpythoncode.py
+++ brokenpythoncode.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-number1 = input("Give me a number.")
-number2 = input("Give me another number.")
+number1 = int(input("Give me a number."))
+number2 = int(input("Give me another number."))
 sum = number1 + number2
 sum = sum * 2
 if sum >= 5:
-    print("(" + number1 + " + " + number2 + ") * 2 is greater than 5.")
+    print("(" + str(number1) + " + " + str(number2) + ") * 2 is greater than 5.")
 else:
     print("no")
